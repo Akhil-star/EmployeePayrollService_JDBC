8b434355747b2fe06dfd458901d6be051dfa1c7f
Akhil-star <akhilakki1305@gmail.com>
2020-11-04 20:08:23 +0530

    Update salary of Employee

--- src/test/java/com/cg/employeedatabase/EmployeePayrollserviceTest.java
+++ src/test/java/com/cg/employeedatabase/EmployeePayrollserviceTest.java
@@ -2,15 +2,24 @@ package com.cg.employeedatabase;
 
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.util.List;
+import static com.cg.employeedatabase.EmployeePayrollService.IOService.DB_IO;
 
 public class EmployeePayrollserviceTest {
     
     @Test
     public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData( EmployeePayrollService.IOService.DB_IO);
-        Assert.assertEquals(3,employeePayrollData.size());
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData( DB_IO);
+        Assert.assertEquals(4,employeePayrollData.size());
+    }
+
+    @Test
+    public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDataBase() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData( DB_IO);
+        employeePayrollService.updateEmployeeSalary("Terissa",3000000.00);
+        boolean result = employeePayrollService.checkEmployeePayrollInsyncWithDB("Terissa");
+        Assert.assertTrue( result );
     }
 }
