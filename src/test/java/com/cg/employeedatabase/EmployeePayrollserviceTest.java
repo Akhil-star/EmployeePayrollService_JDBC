64054e965fc6bc6ac55bea18f3b4722e65959edd
Akhil-star <akhilakki1305@gmail.com>
2020-11-04 18:21:34 +0530

    Read data from database

---
+++ src/test/java/com/cg/employeedatabase/EmployeePayrollserviceTest.java
@@ -0,0 +1,16 @@
+package com.cg.employeedatabase;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class EmployeePayrollserviceTest {
+    
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData( EmployeePayrollService.IOService.DB_IO);
+        Assert.assertEquals(3,employeePayrollData.size());
+    }
+}
